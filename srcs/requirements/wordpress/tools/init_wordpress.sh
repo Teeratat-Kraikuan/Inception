8c21b1ef53ff95268c619101fd9c52a28185da7c
Teeratat-Kraikuan <lovekoonq@gmail.com>
2023-12-07 14:41:52 +0000

    wordpress can run

--- srcs/requirements/wordpress/tools/init_wordpress.sh
+++ srcs/requirements/wordpress/tools/init_wordpress.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p /run/php;
+
 if [ ! -f /var/www/html/wp-config.php ]
 then
     mkdir -p /var/www/html
@@ -12,8 +14,8 @@ then
 	sed -i "s/localhost/$MYSQL_DATABASE_HOST/g" wp-config.php
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
 
-    wp user create $WORDPRESS_ADMIN_USER $WORDPRESS_ADMIN_EMAIL --role=administrator --user_pass=$WORDPRESS_ADMIN_PASSWORD --allow-root
-	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASSWORD --allow-root
+    wp core install --url=$DOMAIN_NAME --title=$TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_PASSWORD --allow-root
 fi
 
 exec "$@"
